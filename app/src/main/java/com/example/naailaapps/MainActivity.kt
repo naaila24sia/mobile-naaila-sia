d678da8e504c54d1aca85ec5edec81bde2f2fc31
lab151-20 <faqihhidayah05@gmail.com>
2026-04-08 16:46:55 +0700

    Pertemuan 4

--- app/src/main/java/com/example/naailaapps/MainActivity.kt
+++ app/src/main/java/com/example/naailaapps/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.naailaapps
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.naailaapps.databinding.ActivityMainBinding
+import com.example.naailaapps.databinding.ActivityThirdBinding
+import com.example.naailaapps.pertemuan_4.FourthActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnToFourth.setOnClickListener {
+            //Mengambil value dari inputNama dan menampilkan di Logcat
+
+            val intent = Intent(this, FourthActivity::class.java)
+
+            intent.putExtra("name", "Politeknik Caltex Riau")
+            intent.putExtra("from", "Rumbai")
+            intent.putExtra("age", 25)
+
+            startActivity(intent)
         }
     }
 }
